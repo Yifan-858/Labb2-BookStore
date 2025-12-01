8d7113ef1b402175944d0bf1f41dae0b33e26aa2
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-12-01 22:44:54 +0100

    Add project files.

---
+++ Labb2-BookStore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb2_BookStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
